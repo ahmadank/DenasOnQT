920b73c63b291bf8ed9d5a8e2c013250b650ffad
Krishna Mathiyaparanam <krishna.mathi11@gmail.com>
2021-04-02 14:02:36 -0400

    Visual changes.

--- source/build-Denas-Desktop_Qt_5_13_0_GCC_64bit-Debug/ui_mainwindow.h
+++ source/build-Denas-Desktop_Qt_5_13_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -14,6 +14,7 @@
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -31,41 +32,50 @@ public:
     QPushButton *pushButton_7;
     QPushButton *pushButton_8;
     QLabel *label;
+    QSlider *horizontalSlider;
+    QLabel *label_2;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(516, 507);
+        MainWindow->resize(516, 472);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         pushButton = new QPushButton(centralWidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(90, 370, 61, 61));
+        pushButton->setGeometry(QRect(90, 310, 61, 61));
         pushButton_2 = new QPushButton(centralWidget);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(90, 250, 61, 61));
+        pushButton_2->setGeometry(QRect(90, 190, 61, 61));
         pushButton_3 = new QPushButton(centralWidget);
         pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
-        pushButton_3->setGeometry(QRect(90, 310, 61, 61));
+        pushButton_3->setGeometry(QRect(90, 250, 61, 61));
         pushButton_4 = new QPushButton(centralWidget);
         pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
-        pushButton_4->setGeometry(QRect(170, 440, 61, 61));
+        pushButton_4->setGeometry(QRect(170, 400, 61, 61));
         pushButton_5 = new QPushButton(centralWidget);
         pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
-        pushButton_5->setGeometry(QRect(90, 440, 61, 61));
+        pushButton_5->setGeometry(QRect(90, 400, 61, 61));
         pushButton_6 = new QPushButton(centralWidget);
         pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
-        pushButton_6->setGeometry(QRect(10, 440, 61, 61));
+        pushButton_6->setGeometry(QRect(10, 400, 61, 61));
         pushButton_7 = new QPushButton(centralWidget);
         pushButton_7->setObjectName(QString::fromUtf8("pushButton_7"));
-        pushButton_7->setGeometry(QRect(150, 310, 61, 61));
+        pushButton_7->setGeometry(QRect(150, 250, 61, 61));
         pushButton_8 = new QPushButton(centralWidget);
         pushButton_8->setObjectName(QString::fromUtf8("pushButton_8"));
-        pushButton_8->setGeometry(QRect(30, 310, 61, 61));
+        pushButton_8->setGeometry(QRect(30, 250, 61, 61));
         label = new QLabel(centralWidget);
         label->setObjectName(QString::fromUtf8("label"));
         label->setGeometry(QRect(10, 10, 231, 231));
+        horizontalSlider = new QSlider(centralWidget);
+        horizontalSlider->setObjectName(QString::fromUtf8("horizontalSlider"));
+        horizontalSlider->setGeometry(QRect(290, 440, 201, 16));
+        horizontalSlider->setOrientation(Qt::Horizontal);
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(300, 420, 64, 17));
         MainWindow->setCentralWidget(centralWidget);
 
         retranslateUi(MainWindow);
@@ -85,6 +95,7 @@ public:
         pushButton_7->setText(QCoreApplication::translate("MainWindow", "RIGHT", nullptr));
         pushButton_8->setText(QCoreApplication::translate("MainWindow", "LEFT", nullptr));
         label->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Battery", nullptr));
     } // retranslateUi
 
 };
